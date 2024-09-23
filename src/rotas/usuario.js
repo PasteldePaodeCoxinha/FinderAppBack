0d8046ef7b00988346dcc1edead238a0d0c887ae
Pastel Caique <caiquepericles.23@gmail.com>
2024-09-22 23:00:16 -0300

    Adicionei a rota para associar um gosto e interesse a um usuario

--- src/rotas/usuario.js
+++ src/rotas/usuario.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {OpenConnection, CloseConnection} = require("../config/database")
+const { OpenConnection, CloseConnection } = require("../config/database")
 
+//
+// POST
 router.post(
     "/cadastro",
-    async(req, res) => {
+    async (req, res) => {
         const conn = await OpenConnection()
 
         const nome = req.body.nome
@@ -18,7 +20,7 @@ router.post(
         try {
             await conn.query(`INSERT INTO usuario (nome, email, senha, datanascimento, profissao, escolaridade, descricao) 
                 VALUES ('${nome}', '${email}', '${senha}', '${datanascimento}', '${profissao}', '${escolaridade}', '${descricao}')`)
-            res.status(200).json({msg: "Cadastrado"})
+            res.status(200).json({ msg: "Cadastrado" })
         } catch (error) {
             console.log(error);
             res.status(500).json({ msg: error })
@@ -28,13 +30,61 @@ router.post(
     }
 )
 
+router.post(
+    "/associarInteGos",
+    async (req, res) => {
+        const conn = await OpenConnection()
+
+        const usuario = req.body.usuario
+        const gostos = req.body.gostos
+        const interesses = req.body.interesses
+        
+        try {
+            let gostosIds = []
+            let intereIds = []
+
+            if (gostos) {
+                const listaGostos = "'" + gostos.join("','") + "'"
+                const gostosQuery = await conn.query(`SELECT id FROM gosto WHERE nome IN (${listaGostos});`)
+                gostosIds = gostosQuery["rows"].map(g => g.id)
+            }
+            if (interesses) {
+                const listaInteresses = "'" + interesses.join("','") + "'"
+                const interessesQuery = await conn.query(`SELECT id FROM interesse WHERE nome IN (${listaInteresses});`)
+                intereIds = interessesQuery["rows"].map(g => g.id)
+            }
+
+            if (gostosIds.length > 0) {
+                const query = `(${usuario},` + gostosIds.join(`),(${usuario},`) + `)`
+
+                await conn.query(`INSERT INTO gostoUsuario(usuario_id, gostos_id) VALUES ${query};`)
+            }
+
+            if (intereIds.length > 0) {
+                const query = `(${usuario},` + intereIds.join(`),(${usuario},`) + `)`
+
+                await conn.query(`INSERT INTO interesseUsuario(usuario_id, interesse_id) VALUES ${query};`)
+            }
+
+            res.status(200).json({msg: "Associado"})
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: error })
+        } finally {
+            CloseConnection(conn)
+        }
+    }
+)
+
+//
+// GERT
 router.get(
     "/lista",
-    async(req, res) => {
+    async (req, res) => {
         const conn = await OpenConnection()
         try {
             const query = await conn.query(`SELECT * FROM usuario;`)
-            res.status(200).json({usuarios: query["rows"]})
+            res.status(200).json({ usuarios: query["rows"] })
         } catch (error) {
             console.log(error);
             res.status(500).json({ msg: error })
