a9b8a95ba7645736a4df3401b34c0127e03a18a3
Pastel Caique <caiquepericles.23@gmail.com>
2024-11-04 22:53:44 -0300

    Fiz a rota de lista de localização

---
+++ js/rotas/localizacao.js
@@ -0,0 +1,59 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+const database_1 = require("../config/database");
+router.post("/cadastrar", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const numero_casa = req.body.numero_casa;
+    const rua = req.body.rua;
+    const bairro = req.body.bairro;
+    const cidade = req.body.cidade;
+    const estado = req.body.estado;
+    const regiao = req.body.regiao;
+    const cep = req.body.cep;
+    const conn = yield (0, database_1.OpenConnection)();
+    try {
+        conn.query(`INSERT INTO localizacao(numero_casa, rua, bairro, cidade, estado, regiao, cep) VALUES(${numero_casa}, '${rua}', '${bairro}', '${cidade}', '${estado}', '${regiao}', '${cep}')`);
+        res.status(200).json({ msg: "Criado" });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error.message });
+    }
+    finally {
+        (0, database_1.CloseConnection)(conn);
+    }
+}));
+router.get("/lista", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    const conn = yield (0, database_1.OpenConnection)();
+    try {
+        const localizacao = (yield conn.query(`SELECT * FROM localizacao WHERE id = ${id}`))["rows"];
+        if (localizacao.length > 0) {
+            res.status(200).json({ localizacao: localizacao[0], msg: "Localização encontrada" });
+        }
+        else {
+            res.status(204).json({ msg: "Essa localização não existe" });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error.message });
+    }
+    finally {
+        (0, database_1.CloseConnection)(conn);
+    }
+}));
+module.exports = router;
--- src/rotas/localizacao.ts
+++ src/rotas/localizacao.ts
@@ -4,21 +4,44 @@ import { OpenConnection, CloseConnection } from "../config/database"
 
 router.post(
     "/cadastrar",
-    async(req, res) => {
+    async (req, res) => {
         const numero_casa = req.body.numero_casa
-        const rua  = req.body.rua
-        const bairro  = req.body.bairro
-        const cidade  = req.body.cidade
-        const estado  = req.body.estado
-        const regiao  = req.body.regiao
-        const cep  = req.body.cep
+        const rua = req.body.rua
+        const bairro = req.body.bairro
+        const cidade = req.body.cidade
+        const estado = req.body.estado
+        const regiao = req.body.regiao
+        const cep = req.body.cep
 
         const conn = await OpenConnection()
         try {
             conn.query(`INSERT INTO localizacao(numero_casa, rua, bairro, cidade, estado, regiao, cep) VALUES(${numero_casa}, '${rua}', '${bairro}', '${cidade}', '${estado}', '${regiao}', '${cep}')`)
-            res.status(200).json({msg: "Criado"})
+            res.status(200).json({ msg: "Criado" })
         } catch (error) {
-            res.status(500).json({msg: (error as Error).message})
+            console.log(error);
+            res.status(500).json({ msg: (error as Error).message })
+        } finally {
+            CloseConnection(conn)
+        }
+    }
+)
+
+router.get(
+    "/lista",
+    async (req, res) => {
+        const id = req.query.id
+
+        const conn = await OpenConnection()
+        try {
+            const localizacao = (await conn.query(`SELECT * FROM localizacao WHERE id = ${id}`))["rows"]
+            if (localizacao.length > 0) {
+                res.status(200).json({ localizacao: localizacao[0], msg: "Localização encontrada" })
+            } else {
+                res.status(204).json({ msg: "Essa localização não existe" })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: (error as Error).message })
         } finally {
             CloseConnection(conn)
         }
