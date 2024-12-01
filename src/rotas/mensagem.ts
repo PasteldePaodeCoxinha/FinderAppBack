3cf9bf36aaf9b49898cd98b9804de7f8426ed4d9
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2024-12-01 08:50:29 -0300

    registrando mensagens como visualizadas

--- src/rotas/mensagem.ts
+++ src/rotas/mensagem.ts
@@ -58,9 +58,34 @@ router.get(
 
         const conn = await OpenConnection()
         try {
-            const listaMsg = (await conn.query(`SELECT * FROM mensagem join chat on chat.id = chat_id WHERE chat_id = ${chatId} and (idUsuario1 = ${usuarioId} or idUsuario2 = ${usuarioId})`))["rows"]
+            const listaMsg = (await conn.query(`SELECT * FROM mensagem join chat on chat.id = chat_id WHERE chat_id = ${chatId} and (idUsuario1 = ${usuarioId} or idUsuario2 = ${usuarioId}) and usuario_id != ${usuarioId}`))["rows"]
             if (listaMsg.length > 0) {
                 res.status(200).json({ mensagens: listaMsg, msg: "Lista de mensagens encontrada" })
+            } else {
+                res.status(404).json({ msg: "Nenhuma mensagem encontrada" })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ msg: (error as Error).message })
+        } finally {
+            CloseConnection(conn)
+        }
+    }
+)
+
+//
+// GET
+router.get(
+    "/marcarVisualizado",
+    async (req, res) => {
+        const chatId = req.query.chatId;
+        const usuarioId = req.query.usuarioId;
+
+        const conn = await OpenConnection()
+        try {
+            const listaMsg = (await conn.query(`update mensagem set visualizado = true where chat_id = ${chatId} and usuario_id = ${usuarioId}`))["rows"]
+            if (listaMsg.length > 0) {
+                res.status(200).json({ mensagens: listaMsg, msg: "Mensagens marcadas como visualizadas" })
             } else {
                 res.status(404).json({ msg: "Nenhuma mensagem encontrada" })
             }
